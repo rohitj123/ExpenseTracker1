cf7ef4aca1f11a833d9b4c30f1a32e4304a81b35
Rohit Jain <145150180+rohitj123@users.noreply.github.com>
2026-08-17 10:35:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,10 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options("*", cors({
+app.options("/{*splat}", cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
-}))
+}));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
